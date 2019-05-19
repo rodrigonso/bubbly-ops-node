de4fef90128d0bcb5733f0803a1e95fac84c67d9
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2019-05-19 18:03:40 -0500

    taet

--- routes/payrolls.js
+++ routes/payrolls.js
@@ -2,8 +2,8 @@ const express = require('express')
 const  { Payroll } = require('../models/payroll-model')
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    const payrolls =  Payroll.find().sort("-date")
+router.get("/", async(req, res) => {
+    const payrolls =  await Payroll.find().sort("-date")
     res.send(payrolls)
 })
 
