27c51611776cdebfef766aa8f64dce5d63f40590
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2019-06-04 15:19:05 -0500

    bug fixes

--- routes/nexmo.js
+++ routes/nexmo.js
@@ -14,12 +14,20 @@ router.post('/', (req, res) => {
     const to = req.body.to
     const text = req.body.text
 
-    try {
-    	nexmo.message.sendSms(from, to, text);
-			res.status(200).send("Your message was sent with success");
-		} catch (ex) {
-			res.status(400).send(ex)
-		}
+    nexmo.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+            console.log(err)
+            res.status(400).send(err)
+        } else {
+            if (responseData.messages[0]['status'] === '0') {
+                console.log("Message sent successfully.")
+                res.status(200).send("Message sent successfully.")
+            } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`)
+                res.status(400).send(`Message failed with error: ${responseData.messages[0]['error-text']}`)
+            }
+        }
+    })
 })
 
 module.exports = router
