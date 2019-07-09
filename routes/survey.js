1210f5f30c9a731c316fbacf005bac080a3c3853
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2019-07-08 19:27:44 -0500

    bug fixes

--- routes/survey.js
+++ routes/survey.js
@@ -7,6 +7,7 @@ router.post('/', (req, res) => {
   delighted.person.create({
     email: req.body.email,
   }).then(response => {
+    console.log(response)
     res.status(200).send(response)
   })
 })
