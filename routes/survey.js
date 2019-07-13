faafcff3da66dd9c104223504e4c5d6d020fc266
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2019-07-12 20:35:11 -0500

    added functionality to delighted webhooks

--- routes/survey.js
+++ routes/survey.js
@@ -6,18 +6,29 @@ const delightedApiKey = config.get('delightedApiKey')
 const delighted = require('delighted')(delightedApiKey)
 
 router.post('/', (req, res) => {
-  console.log(req.body.email)
+  console.log(req.body)
   delighted.person.create({
-    email: req.body.email
+    email: req.body.email,
+    properties: {
+      employeeId: req.body.employeeId,
+      serviceId: req.body.serviceId
+    }
   }).then(response => {
     console.log(response)
     res.status(200).send(response)
   })
 })
 
-router.post('/response', (req, res) => {
+router.post('/response', async(req, res) => {
   console.log(req.body)
-  res.status(200).send(req.body)
+
+  const employee = await employee.finById(req.body.employeeId)
+  if (!employee) res.status(400).send({ msg: 'No employee with given Id' })
+
+  employee.rating = (employee.rating + req.body.score) / 2
+  employee.save()
+
+  res.status(200).send(req.body, employee)
 })
 
 module.exports = router
