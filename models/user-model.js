9ee43a7ad71c6d0179381c52cc2ee851a2c77148
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2019-10-04 16:08:34 -0500

    bug fixes

--- models/user-model.js
+++ models/user-model.js
@@ -1,53 +1,67 @@
-const mongoose = require('mongoose');
-const Joi = require('joi')
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 55
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 55,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 55,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        maxlength: 255
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    employeeId: {
-        type: String,
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 55
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 55,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 55,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 255
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  employeeId: {
+    type: String
+  }
 });
 
-const User = new mongoose.model('User', userSchema);
+const User = new mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(3).max(55).required(),
-        email: Joi.string().email().required(),
-        username: Joi.string().alphanum().min(3).max(30).required(),
-        password: Joi.string().regex(/^[a-zA-Z0-9]{5,30}$/).required()
-    }
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(55)
+      .required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    username: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+    employeeId: Joi.string()
+      .min(24)
+      .required(),
+    password: Joi.string()
+      .regex(/^[a-zA-Z0-9]{5,30}$/)
+      .required()
+  };
 
-    return Joi.validate(user, schema)
+  return Joi.validate(user, schema);
 }
 
 module.exports.User = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,85 +1,106 @@
-const express = require('express');
-const { User, validateUser } = require('../models/user-model');
+const express = require("express");
+const { User, validateUser } = require("../models/user-model");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const config = require('config');
-const jwt = require('jsonwebtoken');
-
+const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 // @route   POST api/auth
 // @desc    Login user
 // @access  Public
-router.post('/login', async(req, res) => {
-    const { name, email, username, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { name, email, username, password } = req.body;
 
-    // check for existing user
-    const existingUser = await User.findOne({ username: req.body.username });
-    if (!existingUser) return res.status(404).send("User not found");
-    
-    // validate user password
-    const isMatch = await bcrypt.compare(password, existingUser.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
-    console.log(existingUser)
-    jwt.sign(
-        { id: existingUser.id, username: existingUser.username, isAdmin: existingUser.isAdmin, name: existingUser.name, email: existingUser.email, employeeId: existingUser.employeeId },
-        config.get("jwtSecret"),
-        { expiresIn: 3600 },
-        (err, token) => {
-            if (err) throw err;
-            res.json({
-                token,
-                user: { id: existingUser.id, name: existingUser.name, username: existingUser.username, email: existingUser.email, isAdmin: existingUser.isAdmin, employeeId: existingUser.employeeId }
-            })
+  // check for existing user
+  const existingUser = await User.findOne({ username: req.body.username });
+  if (!existingUser) return res.status(404).send("User not found");
+
+  // validate user password
+  const isMatch = await bcrypt.compare(password, existingUser.password);
+  if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
+  console.log(existingUser);
+  jwt.sign(
+    {
+      id: existingUser.id,
+      username: existingUser.username,
+      isAdmin: existingUser.isAdmin,
+      name: existingUser.name,
+      email: existingUser.email,
+      employeeId: existingUser.employeeId
+    },
+    config.get("jwtSecret"),
+    { expiresIn: 3600 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({
+        token,
+        user: {
+          id: existingUser.id,
+          name: existingUser.name,
+          username: existingUser.username,
+          email: existingUser.email,
+          isAdmin: existingUser.isAdmin,
+          employeeId: existingUser.employeeId
         }
-    )
+      });
+    }
+  );
 });
 
-// @route   POST api/auth
-// @desc    Register new user
-// @access  Public
-router.post('/register', async(req, res) => {
-    const { name, email, username, password } = req.body;
+router.post("/register", async (req, res) => {
+  const { name, email, username, password } = req.body;
 
-    // validate user
-    const { error } = validateUser(req.body);
-    if (error) {
-        console.log(error.details[0].message)
-        return res.status(401).send(error.details[0].message)
-    } 
+  // validate user
+  const { error } = validateUser(req.body);
+  if (error) {
+    console.log(error.details[0].message);
+    return res.status(400).send(error.details[0].message);
+  }
 
-    // check for existing user
-    const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(402).send({ msg: "User already exists" });
+  // check for existing user
+  const existingUser = await User.findOne({ email });
+  if (existingUser) return res.status(402).send({ msg: "User already exists" });
 
-    // create new user
-    const newUser = await new User({
-        name,
-        email,
-        username,
-        password
-    });
+  // create new user
+  const newUser = await new User({
+    name,
+    email,
+    username,
+    password
+  });
 
-    // create salt & hash password
-    const salt = await bcrypt.genSalt(10);
-    bcrypt.hash(newUser.password, salt, (err, hash) => {
+  // create salt & hash password
+  const salt = await bcrypt.genSalt(10);
+  bcrypt.hash(newUser.password, salt, (err, hash) => {
+    if (err) throw err;
+    newUser.password = hash;
+    newUser.save();
+
+    jwt.sign(
+      {
+        id: newUser.id,
+        username: newUser.username,
+        isAdmin: newUser.isAdmin,
+        name: newUser.name,
+        email: newUser.email
+      },
+      config.get("jwtSecret"),
+      { expiresIn: 3600 },
+      (err, token) => {
+        console.log(err, token);
         if (err) throw err;
-        newUser.password = hash;
-        newUser.save();
-
-        jwt.sign(
-            { id: newUser.id, username: newUser.username, isAdmin: newUser.isAdmin, name: newUser.name, email: newUser.email },
-            config.get("jwtSecret"),
-            { expiresIn: 3600 },
-            (err, token) => {
-                console.log(err, token)
-                if (err) throw err;
-                return res.status(200).send({
-                    token,
-                    user: { id: newUser.id, name: newUser.name, username: newUser.username, email: newUser.email }
-                });
-            }
-        )
-    })
-})
+        return res.status(200).send({
+          token,
+          user: {
+            id: newUser.id,
+            name: newUser.name,
+            username: newUser.username,
+            email: newUser.email
+          }
+        });
+      }
+    );
+  });
+});
 
 module.exports = router;
